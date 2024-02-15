886b628217f2376b93e5a5a20e46c71a92e490b9
Gopi Jambucha <82703532+gopi-png@users.noreply.github.com>
2024-02-15 18:28:36 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Exit immediately if any command exits with a non-zero status
+set -e
+
+# Switch to the root user
+sudo su -
+
+# Pull the Docker image
+docker pull gopi303/codepipeline-demo-final
+
+# Run the Docker container
+docker run -d -p 5000:5000 gopi303/codepipeline-demo-final
